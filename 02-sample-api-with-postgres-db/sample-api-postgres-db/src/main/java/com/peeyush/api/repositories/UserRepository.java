234ccc344cc5732b9de169e60efa20b44bde5cc8
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-01-18 21:30:02 +0100

    Adding native query

--- 02-sample-api-with-postgres-db/sample-api-postgres-db/src/main/java/com/peeyush/api/repositories/UserRepository.java
+++ 02-sample-api-with-postgres-db/sample-api-postgres-db/src/main/java/com/peeyush/api/repositories/UserRepository.java
@@ -2,12 +2,18 @@ package com.peeyush.api.repositories;
 
 import com.peeyush.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     List<User> findByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM public.users a INNER JOIN public.contacts b ON a.user_id = b.user_id WHERE a.user_id = :userId")
+    Optional<User> findByIdNativeQuery(@Param("userId") Integer userId);
     
     //List<User> findByNameAndAge(String name, int age);
 }
